5ef1555815f60a0aa06b2473f17509d801f68010
Dmitry Lyalyaev <fruktime@altlinux.org>
2023-07-28 09:12:56 +0300

    add licence text in the Header.tsx and Sidebar.tsx files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,23 @@
+/*
+Management Erratas
+Copyright (C) 2021-2023  BaseALT Ltd
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theц
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import React, {FC} from 'react';
 import {
-    Avatar,
     Dropdown,
     DropdownGroup,
     DropdownItem,
@@ -20,7 +37,6 @@ import {
 import BarsIcon from "@patternfly/react-icons/dist/esm/icons/bars-icon";
 import {AuthContext} from "../context/AuthProvide";
 import {observer} from "mobx-react";
-// import imgAvatar from '@patternfly/react-core/src/components/Avatar/examples/avatarImg.svg';
 
 const Header: FC = () => {
     const {auth} = React.useContext(AuthContext);
@@ -87,7 +103,7 @@ const Header: FC = () => {
                 default: 'hidden',
                 md: 'visible'
             }}>
-                <Dropdown isFullHeight onSelect={onDropdownSelect} isOpen={isDropdownOpen} toggle={<DropdownToggle icon={<Avatar src={""} alt="Avatar" />} onToggle={onDropdownToggle}>
+                <Dropdown isFullHeight onSelect={onDropdownSelect} isOpen={isDropdownOpen} toggle={<DropdownToggle onToggle={onDropdownToggle}>
                     {auth.user.nickname}
                 </DropdownToggle>} dropdownItems={userDropdownItems} />
             </ToolbarItem>
