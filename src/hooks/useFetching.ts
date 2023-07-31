9b08d8ba6c7c9bc29be77c2d213659a3ae3594ac
Dmitry Lyalyaev <fruktime@altlinux.org>
2023-07-31 12:23:31 +0300

    services: create a custom React hook to fetch an API

---
+++ src/hooks/useFetching.ts
@@ -0,0 +1,49 @@
+/*
+Management Erratas
+Copyright (C) 2021-2023  BaseALT Ltd
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theц
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import {useState} from "react";
+
+export type TFetch = {
+    fetching: any;
+    isLoading: boolean;
+    error: string;
+}
+
+
+export const useFetching = (callback: (...args: any[]) => any): TFetch => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+
+    const fetching = async (...args: any[]) => {
+        try {
+            setIsLoading(true)
+            await callback(...args)
+            setError('')
+        } catch (e) {
+            // if (e.response.status === 401) {
+            //     Auth.removeAccessToken()
+            //     setIsAuth(false)
+            // };;
+            setError(e.message);
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return {fetching, isLoading, error}
+}
