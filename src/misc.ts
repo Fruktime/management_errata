e6645554ca4da55f0fc3eeb09e9c3306ae13bb48
Dmitry Lyalyaev <fruktime@altlinux.org>
2023-09-15 12:06:13 +0300

    misc: update constants

--- src/misc.ts
+++ src/misc.ts
@@ -22,7 +22,15 @@ export const constants = {
     VALID_ERR_TYPE: "error" as "error",
     VALID_SUC_TYPE: "success" as "success",
     ERRATA_PACKAGES_PREFIX: "ALT-PU-",
-    ERRATA_BRANCH_PREFIX: "ALT-BU-"
+    ERRATA_BRANCH_PREFIX: "ALT-BU-",
+    CVE_PREFIX: "CVE-",
+    BDU_PREFIX: "BDU:",
+    MFSA_PREFIX: "MFSA",
+    PACKAGES_URL: "https://packages.altlinux.org/en",
+    BUGZILLA_URL: "https://bugzilla.altlinux.org",
+    NVD_CVE_URL: "https://nvd.nist.gov/vuln/detail",
+    FSTEC_BDU_URL: "https://bdu.fstec.ru/vul",
+    MFSA_URL: "https://www.mozilla.org/en-US/security/advisories",
 }
 
 /**
